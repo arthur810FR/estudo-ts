d5e1353ca1a9aeb6f9de11cfffc0a6a22e6ea5fd
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-01-15 09:54:06 -0300

    Estudo do tipo number e organização de pastas

---
+++ fundamentos/index.js
@@ -0,0 +1,8 @@
+// 1 - Numbers
+var x = 10;
+x = 16;
+console.log(x);
+console.log(typeof x);
+var y = 15.7323189;
+console.log(typeof y);
+console.log(y.toPrecision(3));
---
+++ fundamentos/index.ts
@@ -0,0 +1,12 @@
+// 1 - Numbers
+let x: number = 10
+
+x = 16;
+
+console.log(x)
+console.log(typeof x)
+
+const y: number = 15.7323189
+
+console.log(typeof y)
+console.log(y.toPrecision(3))
