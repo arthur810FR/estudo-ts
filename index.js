9f52ce01055225dfa81d9bb7c6ad762b44421e61
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-01-15 09:40:39 -0300

    Subindo introducao ao TS

---
+++ index.js
@@ -0,0 +1,13 @@
+var firstName = "Arthur";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(firstName);
+var number1 = 10;
+var number2 = 10;
+function soma(number1, number2) {
+    console.log(number1 + number2);
+}
+soma(number1, number2);
---
+++ index.ts
@@ -0,0 +1,18 @@
+const firstName = "Arthur";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string){
+    console.log("Olá " + name);
+}
+
+greeting(firstName);
+
+const number1 = 10;
+const number2 = 10;
+
+function soma(number1: number, number2: number){
+    console.log(number1 + number2);
+}
+
+soma(number1, number2);
