d13ce7577bfdbfbb2d9788d57be3782994b3f6ea
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-01-15 10:06:59 -0300

    Estudo de tipos no TS

--- fundamentos/index.js
+++ fundamentos/index.js
@@ -6,3 +6,18 @@ console.log(typeof x);
 var y = 15.7323189;
 console.log(typeof y);
 console.log(y.toPrecision(3));
+// 2 - Strings
+var firstName = "Arthur";
+console.log(firstName.toUpperCase());
+console.log(typeof firstName);
+var lastName = "Francisco";
+console.log(lastName.toUpperCase());
+console.log(typeof lastName);
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+// 3 - Booleans
+var isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+isStudent = false;
+console.log(isStudent);
--- fundamentos/index.ts
+++ fundamentos/index.ts
@@ -10,3 +10,26 @@ const y: number = 15.7323189
 
 console.log(typeof y)
 console.log(y.toPrecision(3))
+
+// 2 - Strings
+
+let firstName: string = "Arthur";
+console.log(firstName.toUpperCase())
+console.log(typeof firstName)
+
+let lastName: string = "Francisco";
+console.log(lastName.toUpperCase())
+console.log(typeof lastName)
+
+const fullName: string = firstName + " " + lastName
+console.log(fullName)
+
+// 3 - Booleans
+
+let isStudent: boolean = true;
+console.log(isStudent)
+console.log(typeof isStudent)
+
+isStudent = false;
+
+console.log(isStudent)
